9983f30bf07ccbcfd6946689a94020068f4c49c2
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-07-12 06:26:38 +0200

    Initial commit

---
+++ Source/UE5_CPP_SpaceShooter/UE5_CPP_SpaceShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_CPP_SpaceShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_CPP_SPACESHOOTER_API AUE5_CPP_SpaceShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
