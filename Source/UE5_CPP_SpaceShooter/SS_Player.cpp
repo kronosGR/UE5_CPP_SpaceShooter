439b4dc33d7357cef82c0983e1006e117fd9fe22
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-07-14 06:31:01 +0200

    added player movement

--- Source/UE5_CPP_SpaceShooter/SS_Player.cpp
+++ Source/UE5_CPP_SpaceShooter/SS_Player.cpp
@@ -10,7 +10,7 @@
 // Sets default values
 ASS_Player::ASS_Player()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	AutoPossessPlayer = EAutoReceiveInput::Player0;
@@ -22,21 +22,45 @@ ASS_Player::ASS_Player()
 	ExplosionFX = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ExplosionFX"));
 	DeathExplosionSound = CreateDefaultSubobject<UAudioComponent>(TEXT("DeathExplosionSound"));
 
-	CollisionComponent->AttachToComponent(RootComponent,FAttachmentTransformRules::KeepRelativeTransform);
-	ShipMesh->AttachToComponent(RootComponent,FAttachmentTransformRules::KeepRelativeTransform);
-	ParticleSystem->AttachToComponent(RootComponent,FAttachmentTransformRules::KeepRelativeTransform);
-	DeathExplosionSound->AttachToComponent(RootComponent,FAttachmentTransformRules::KeepRelativeTransform);
-	ExplosionFX->AttachToComponent(RootComponent,FAttachmentTransformRules::KeepRelativeTransform);
+	CollisionComponent->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	ShipMesh->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	ParticleSystem->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	DeathExplosionSound->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+	ExplosionFX->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
 
-	
-	
+	MaxVelocity = 250.f;
+	Current_X_Velocity = 0.f;
+	Current_Y_Velocity = 0.f;
+
+	bIsFiring = false;
+	WeaponFireRate = 0.25f;
+	TimeSinceLastShot = 0.f;
+
+	PlayerScore = 0.f;
 }
+
 
 // Called when the game starts or when spawned
 void ASS_Player::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	Current_Location = this->GetActorLocation();
+	Current_Rotation = this->GetActorRotation();
+
+	bHit = false;
+	bDead = false;
+
+	ExplosionFX->Deactivate();
+	DeathExplosionSound->Deactivate();
+
+	Max_Health = 100.f;
+	Current_Health = 100.f;
+
+	Max_Armor = 100.f;
+	Current_Armor = 100.f;
+
+	OnActorBeginOverlap.AddDynamic(this, &ASS_Player::OnBeginOverlap);
 }
 
 // Called every frame
@@ -44,6 +68,14 @@ void ASS_Player::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (Current_X_Velocity != 0.f || Current_Y_Velocity != 0.f)
+	{
+		New_Location = FVector(Current_Location.X + (Current_X_Velocity * DeltaTime),
+		                       Current_Location.Y + (Current_Y_Velocity * DeltaTime), 0.f);
+
+		this->SetActorLocation(New_Location);
+		Current_Location = New_Location;
+	}
 }
 
 // Called to bind functionality to input
@@ -51,5 +83,38 @@ void ASS_Player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASS_Player::MoveRight);
+	PlayerInputComponent->BindAxis("MoveUp", this, &ASS_Player::MoveUp);
+	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &ASS_Player::FireWeapon);
 }
 
+
+void ASS_Player::MoveRight(float AxisValue)
+{
+	Current_X_Velocity = MaxVelocity * AxisValue;
+}
+
+void ASS_Player::MoveUp(float AxisValue)
+{
+	Current_Y_Velocity = MaxVelocity * AxisValue;
+}
+
+void ASS_Player::FireWeapon()
+{
+}
+
+void ASS_Player::StartFiring()
+{
+}
+
+void ASS_Player::StopFiring()
+{
+}
+
+void ASS_Player::OnBeginOverlap(AActor* PlayerActor, AActor* OtherActor)
+{
+}
+
+void ASS_Player::CollectablePickUp()
+{
+}
--- Source/UE5_CPP_SpaceShooter/SS_Player.h
+++ Source/UE5_CPP_SpaceShooter/SS_Player.h
@@ -32,6 +32,63 @@ public:
 
 	UPROPERTY(EditAnywhere)
 	UAudioComponent* DeathExplosionSound;
+
+	UPROPERTY(EditAnywhere)
+	float Field_Width;
+	
+	UPROPERTY(EditAnywhere)
+	float Field_Height;
+	
+	UPROPERTY(BlueprintReadOnly)
+	float Max_Health;
+	
+	UPROPERTY(BlueprintReadOnly)
+	float Current_Health;
+	
+	UPROPERTY(BlueprintReadOnly)
+	float Max_Armor;
+	
+	UPROPERTY(BlueprintReadOnly)
+	float Current_Armor;
+
+	void MoveRight(float AxisValue);
+	void MoveUp(float AxisValue);
+
+	void FireWeapon();
+	void StartFiring();
+	void StopFiring();
+	
+
+	UPROPERTY(EditAnywhere)
+	float MaxVelocity;
+
+	bool bIsFiring;
+	float WeaponFireRate;
+	float TimeSinceLastShot;
+
+	bool bHit;
+	bool bDead;
+
+	float Current_X_Velocity;
+	float Current_Y_Velocity;
+
+	
+	UPROPERTY(BlueprintReadOnly)
+	float PlayerScore;
+
+	FVector Current_Location;
+	FRotator Current_Rotation;
+	FVector New_Location;
+
+	UFUNCTION()
+	void OnBeginOverlap(AActor* PlayerActor, AActor* OtherActor);
+
+	
+	// UPROPERTY(EditAnywhere)
+	// TSubclassOf<AProjectile> WeaponProjectile_BP;
+
+	void CollectablePickUp();
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
