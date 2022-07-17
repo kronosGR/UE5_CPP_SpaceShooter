9cf92bbe24bf830daa9df0fd428cacbe2d764be1
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-07-17 06:46:02 +0200

    added gamemode BP and set it as default

--- Source/UE5_CPP_SpaceShooter/SS_GameMode.cpp
+++ Source/UE5_CPP_SpaceShooter/SS_GameMode.cpp
@@ -8,14 +8,14 @@
 ASS_GameMode::ASS_GameMode()
 {
 	SpawnTimer = 0.f;
-	
+
 	MusicTrack = CreateDefaultSubobject<UAudioComponent>(TEXT("Music"));
 	MusicTrack->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
 
 	AsteroidSpawnLoc = FVector(1000.f, 1000.f, 1000.f);
 	AsteroidSpawnRot = FRotator(0.f, 0.f, 0.f);
 
-	ThisWorld=GetWorld();
+	ThisWorld = GetWorld();
 }
 
 void ASS_GameMode::BeginPlay()
@@ -29,6 +29,38 @@ void ASS_GameMode::BeginPlay()
 void ASS_GameMode::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
+
+	SpawnTimer = FMath::RandRange(0, 1000);
+
+	if (Enemy)
+	{
+		if (SpawnTimer > 990)
+		{
+			FVector EnemySpawnLoc(0.f, 0.f, 0.f);
+			FRotator EnemySpawnRot(0.f, 0.f, 0.f);
+
+			AActor* NewEnemy = ThisWorld->SpawnActor(Enemy, &EnemySpawnLoc, &EnemySpawnRot, AsteroidSpawnParameters);
+
+			if (NewEnemy)
+			{
+				NewEnemy->SetActorLocation(FVector(1000.f, FMath::RandRange(-500, 500), 0.f));
+			}
+		}
+	}
+
+	if (HazardTemplate)
+	{
+		if (SpawnTimer > 995)
+		{
+			AActor* NewHazard = ThisWorld->SpawnActor(HazardTemplate, &AsteroidSpawnLoc, &AsteroidSpawnRot,
+			                                          AsteroidSpawnParameters);
+
+			if (NewHazard)
+			{
+				NewHazard->SetActorLocation(FVector(1000.f, 1000.f, 1000.f));
+			}
+		}
+	}
 }
 
 TSubclassOf<AHazard> ASS_GameMode::GetHazardTemplate()
